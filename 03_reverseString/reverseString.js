43138d8e14732799a9f70842c7ce1bb5ca3ec24f
ZoeChen <28507096+yhcyc5@users.noreply.github.com>
2023-08-16 22:17:14 +0800

    Complete 01~04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let length = str.length;
+    let output = '';
+    for (let i = length-1; i >= 0; i--) {
+        output += str[i];
+    }
+    return output;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...myArray) {
+    
+    const basicArray = myArray.shift();
+    const deleteWord = myArray;
+    const outputArray = [];
+    for (let bElement of basicArray){
+        let is_del = false;
+        deleteWord.forEach(dElement => {
+            if (bElement === dElement) {
+                is_del = true;
+            }
+        });
+        if (is_del === false) {
+            outputArray.push(bElement);
+        }
+    }
+    return outputArray;
 };
 
 // Do not edit below this line
